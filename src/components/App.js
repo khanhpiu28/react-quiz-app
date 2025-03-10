dd384b737ef05941cf6d77173056a2d5ba463448
HeoKhoDai <baokhanh20802@gmail.com>
2025-03-10 21:43:23 +0700

    restarting a quiz

--- src/components/App.js
+++ src/components/App.js
@@ -58,6 +58,20 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       };
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+      };
+    // return {
+    //   ...state,
+    //   points: 0,
+    //   highscore: 0,
+    //   index: 0,
+    //   answer: null,
+    //   status: "ready",
+    // };
 
     default:
       throw new Error("Action unknown");
@@ -117,6 +131,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
