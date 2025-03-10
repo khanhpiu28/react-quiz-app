a674914d3541b91da9221680fba190877d90d6a5
HeoKhoDai <baokhanh20802@gmail.com>
2025-03-10 19:17:39 +0700

    moving to the next question

--- src/components/Option.js
+++ src/components/Option.js
@@ -4,7 +4,7 @@ function Option({ question, dispatch, answer }) {
     <div className="options">
       {question.options.map((option, index) => (
         <button
-          className={`btn btn-option ${index === answer ? "answer" : ""}${
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
             hasAnswered
               ? index === question.correctOption
                 ? "correct"
